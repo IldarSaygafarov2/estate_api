206fbcbf6cee4ec6c2b6f4f557e4345962a30049
IldarSaygafarov2 <sayildar17@gmail.com>
2024-11-21 18:04:58 +0500

    added fixes

--- backend/api/v1/routes/objects.py
+++ backend/api/v1/routes/objects.py
@@ -1,6 +1,16 @@
 from fastapi import APIRouter, Depends
 from backend.app.config import config
 from backend.core.interfaces.objects import ObjectDTO
+from backend.core.interfaces.balcony import BalconyDTO
+from backend.core.interfaces.condition import ConditionDTO
+from backend.core.interfaces.type import TypeDTO
+from backend.core.interfaces.room import RoomDTO
+from backend.core.interfaces.floor import FloorDTO
+from backend.core.interfaces.storey import StoreyDTO
+from backend.core.interfaces.district import DistrictDTO
+
+
+
 from backend.app.dependencies import get_repo
 from typing import Annotated
 from infrastructure.database.repo.requests import RequestsRepo
@@ -16,13 +26,13 @@ router = APIRouter(
 async def get_objects(
     repo: Annotated[RequestsRepo, Depends(get_repo)]
 ) -> ObjectDTO:
-    balconies = await repo.balcony.get_balconies()
-    conditions = await repo.condition.get_conditions()
-    types = await repo.type.get_all()
-    rooms = await repo.room.get_all()
-    floors = await repo.floor.get_all()
-    districts = await repo.district.get_districts()
-    storeys = await repo.storey.get_all()
+    balconies = [BalconyDTO.model_validate(balcony, from_attributes=True) for balcony in await repo.balcony.get_balconies()]
+    conditions = [ConditionDTO.model_validate(condition, from_attributes=True) for condition in await repo.condition.get_conditions()]
+    types = [TypeDTO.model_validate(type_obj, from_attributes=True) for type_obj in await repo.type.get_all()]
+    rooms = [RoomDTO.model_validate(room, from_attributes=True) for room in await repo.room.get_all()]
+    floors = [FloorDTO.model_validate(floor, from_attributes=True) for floor in await repo.floor.get_all()]
+    districts = [DistrictDTO.model_validate(district, from_attributes=True) for district in await repo.district.get_districts()]
+    storeys = [StoreyDTO.model_validate(storey, from_attributes=True) for storey in await repo.storey.get_all()]
 
     obj = ObjectDTO(
         balconies=balconies,
