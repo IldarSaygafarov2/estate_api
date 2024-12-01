2d6e98abeab540ea756fdf271df76dc48e2dbc8a
IldarSaygafarov2 <sayildar17@gmail.com>
2024-12-01 13:08:24 +0500

    added fixes

--- backend/core/filters/estate.py
+++ backend/core/filters/estate.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field
 class EstateFilter(BaseModel):
     condition: Optional[str] = Field(None)
     district: Optional[str] = Field(None)
-    rooms: Optional[str] = Field(None)
+    room: Optional[str] = Field(None)
     type: Optional[str] = Field(None)
     price_min: Optional[int] = Field(None)
     price_max: Optional[int] = Field(None)
